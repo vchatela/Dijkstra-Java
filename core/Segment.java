8484db9c3bfc72efac84343443b6fbcdf67671f6
valentin <valentindu64@hotmail.fr>
2015-03-30 10:17:58 +0200

    Segment

--- core/Segment.java
+++ core/Segment.java
@@ -1,5 +1,4 @@
 package core;
-package base;
 
 public class Segment {
 
@@ -13,12 +12,12 @@ public class Segment {
 	/*
 	 * Constructeurs
 	*/
-	public Arc(){
+	public Segment(){
 		this.delta_long = 0;
 		this.delta_lat = 0;
 	}
 	
-	public Arc(float delta_long, float delta_lat){
+	public Segment(float delta_long, float delta_lat){
 		this.delta_long = delta_long;
 		this.delta_lat = delta_lat;
 	}
