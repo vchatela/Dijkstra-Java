1eb2eada0003373a57a408c4f60bc96094a2d6c2
Valentin <valentindu64@hotmail.fr>
2015-04-29 16:02:12 +0200

    PCC

---
+++ core/Label_Star.java
@@ -0,0 +1,27 @@
+package core;
+
+/**
+ * Created by valentin on 4/29/15.
+ */
+public class Label_Star extends Label{
+
+    private double cout_oiseau;
+
+    public Label_Star(Node node){
+        super(node);
+        this.cout_oiseau=Float.POSITIVE_INFINITY;
+    }
+
+    public String toString(){
+        return "Sommet N"+this.getNum_node()+" Cout: "+this.getCout()+ " Cout oiseau : "+ this.cout_oiseau;
+    }
+    public double getCout_oiseau() {
+        return cout_oiseau;
+    }
+
+    public void setCout_oiseau(double cout_oiseau) {
+        this.cout_oiseau = cout_oiseau;
+    }
+
+
+}
